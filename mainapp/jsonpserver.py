1a6df9b0918e7388aec3a8fb94c4345c5ec3f3c6
Veselin <penev.veselin@gmail.com>
2015-09-03 21:19:26 +0300

    отладка

--- mainapp/jsonpserver.py
+++ mainapp/jsonpserver.py
@@ -473,7 +473,7 @@ class JSONPEntryPoint(View):
                         except:
                             pass
                         try:
-                            print (urllib.unquote(hostname)).encode(locale.getpreferredencoding())
+                            print (urllib.unquote(hostname.decode('utf-8'))).encode(locale.getpreferredencoding())
                         except:
                             pass
                         if widget.site_url.count(hostname):
